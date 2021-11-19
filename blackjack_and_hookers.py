9aa133f8e17690c3e3a37f2836922042f82e6ac2
Robert Duggan <robert.duggan@live.com>
2021-11-18 21:08:08 -0500

    add player class

--- blackjack_and_hookers.py
+++ blackjack_and_hookers.py
@@ -1,9 +1,23 @@
 import random
 
+class Player:
+    def __init__(self, cards, score = 0, chips = 200):
+        self.cards = cards
+        self.score = score
+        self.chips = chips
+
+
 card_suit = ["diamond", "club", "heart", "spade"]
 card_val = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 card_deck = {}
 for suit in card_suit:
     card_deck[suit] = card_val
 
-print(card_deck)
+cards = []
+
+for i in range(1,53):
+    cards.append(i)
+
+player1 = Player([1,2])
+
+print(cards)

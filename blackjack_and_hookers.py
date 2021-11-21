bdaa284ef9e1252e0b583ac009ca2d6a235a7dbb
Robert Duggan <rduggan@pop-os.localdomain>
2021-11-21 11:02:19 -0500

    added hit

--- blackjack_and_hookers.py
+++ blackjack_and_hookers.py
@@ -39,11 +39,10 @@ def card_calc(card):
 
 # make it so that it goes round twice instead of both cards at once maybe counter = 0 with while loop
 def initial_deal():
-    for player in players.keys():
-        card1 = card_calc(cards.pop(random.randint(0,len(cards)-1)))
-        card2 = card_calc(cards.pop(random.randint(0,len(cards)-1)))
-        players[player].append(card1)
-        players[player].append(card2)
+    for i in range(1,3):
+        for player in players.keys():
+            card1 = card_calc(cards.pop(random.randint(0,len(cards)-1)))
+            players[player].append(card1)
 
 print("Please wait while I deal the cards.")
 initial_deal()
@@ -60,7 +59,7 @@ def find_suits(card):
     elif card > 39 and card <= 52:
         return "spades"
 
-def convert_for_is_face(card):
+def convert_to_only_suit(card):
     if card >= 1 and card <= 13:
         return card
     elif card > 13 and card <= 26:
@@ -73,16 +72,16 @@ def convert_for_is_face(card):
         return card
 
 def is_face(card):
-    new_card = convert_for_is_face(card)
-    if new_card > 9 and new_card <= 13:
-        if new_card == 10:
+    new_card = convert_to_only_suit(card)
+    if new_card == 1:
+        return "ace"
+    elif new_card > 10 and new_card <= 13:
+        if new_card == 11:
             return "jack"
-        elif new_card == 11:
-            return "queen"
         elif new_card == 12:
-            return "king"
+            return "queen"
         elif new_card == 13:
-            return "ace"
+            return "king"
     else:
         return new_card
 
@@ -92,51 +91,74 @@ def get_cards(players_cards):
     for hand, card in players_cards.items():
         temp_hand = []
         for card in players_cards[hand]:
-            val = 0
-            # 1 - 9
-            if card >= 1 and card <= 9:
-                temp_hand.append(card)
+            converted_card = convert_to_only_suit(card)
+            if converted_card == 1:
+                temp_hand.append(11)
+            # 2 - 9
+            elif converted_card >= 2 and converted_card <= 9:
+                temp_hand.append(converted_card)
             # 10 - 13
-            elif card > 9 and card <=13:
-                val = 10
-                temp_hand.append(val)
-            # 13 - 22
-            elif card <= 13 * 2 - 4:
-                val = card - 13
-                temp_hand.append(val)
-            # 23 - 26
-            elif card > 13 * 2 - 4 and card <= 13 * 2:
-                val = 10
-                temp_hand.append(val)
-            # 27 - 35
-            elif card <= 13 * 3 - 4:
-                val = card - (13 * 2)
-                temp_hand.append(val)
-            # 36 - 39
-            elif card > 13 * 3 - 4 and card <= 13 * 3:
-                val = 10
-                temp_hand.append(val)
-            # 40 - 48
-            elif card <= 13 * 4 - 4:
-                val = card - 13 * 3
-                temp_hand.append(val)
-            # 49 - 52
-            elif card > 13 * 4 - 4:
-                val = 10
-                temp_hand.append(val)
+            elif converted_card > 9 and converted_card <=13:
+                temp_hand.append(10)
         temp_hands.append(temp_hand)
     return temp_hands
 
 #this is used to get the cards actual value to determine winner
 player_cards = get_cards(players)
+print(player_cards)
 
-string = "You currently have "
+def card_string(card_list):
+    card_string = "You currently have the "
+    if len(card_list) == 1:
+        card_string += str(is_face(card_list)) + " of " + str(find_suits(card_list)) + "."
+    elif len(card_list) == 2:
+        card_string += str(is_face(card_list[0])).capitalize() + " of " + str(find_suits(card_list[0])).capitalize() + " and " + str(is_face(card_list[1])).capitalize() + " of " + str(find_suits(card_list[1])).capitalize() + "."
+    elif len(card_list) > 2:
+        for i in range(0, len(card_list)):
+            card_face = str(is_face(card_string[i])).capitalize()
+            card_suit = str(find_suits(card_string[i])).capitalize()
+            card_string += card_face + " of " + card_suit + " and, "
+        card_string += str(is_face(card_string[-1])).capitalize() + " of " + str(find_suits(card_list[-1])) +  "."
+    return card_string
 
-for card in players['player 1']:
-    string += str(is_face(card)) + " of " +  str(find_suits(card)) + " and "
+print(card_string(players['player 1']))
 
-print(string)
+def hit(player):
+    add_card = card_calc(cards.pop(random.randint(0,len(cards)-1)))
+    players[player].append(add_card)
 
+def find_winner(player_cards):
+    print(player_cards)
+    player_sums = []
+    for hand in player_cards:
+        player_sums.append(sum(hand))
+    return player_sums
+
+decision = input("Would you like to hit or stand? ").lower()
+print(decision)
+
+if decision == "hit":
+    hit('player 1')
+    winner = find_winner(get_cards(players))
+    print(card_string(players['player 1']))
+    print("Other players stand.")
+    again = input("Would you like to hit again?").lower()
+    if again == "yes":
+        hit('player 1')
+        winner = find_winner(get_cards(players))
+    if winner[0] > 21:
+        print("BUST! Try again.")
+    highest_hand = 0
+    winning_player = ""
+    for i in range(len(winner)):
+        if winner[i] > highest_hand:
+            highest_hand = winner[i]
+            winning_player = f"player {i}"
+    print(f"{winning_player} wins with {highest_hand}!")
+else:
+    print(find_winner(player_cards))
+
+# find end result and display winner 
 # to-do
 # add ablility to hit, pass, double-down (computer is stuck with what it has, maybe some small AI like if total is less than 13 hit)
 # find end result and display winner 
